f16d5e89102bdfcaec7f9d25073c746997b03464
adam-sagar <adamschrissagar@gmail.com>
2023-04-24 14:44:03 +1200

    Fixed card layout centering issues when resizing

--- CAPSTONE/front-end/src/components/Post.jsx
+++ CAPSTONE/front-end/src/components/Post.jsx
@@ -12,7 +12,7 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import axios from 'axios';
-import { Grid } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 
 function Post() {
 
@@ -36,66 +36,59 @@ function Post() {
 
     return (
         
-        <Grid container spacing={4}>
-            {posts.map((post, index) => (
-                <Grid item xs={12} sm={6} md={4} key={index} >
-                    <Card sx={{ maxWidth: 300, margin: 3, borderRadius: 2 }}>
-                        <CardMedia
-                            sx={{ height: 200 }}
-                            image="/images/discgolfaction.jpg"
-                            title="disc"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="div" sx={{ mb: 1, fontFamily: 'Roboto Condensed, sans-serif' }}>
-                                { post.isFound ? "Found" : "Lost" }
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary" sx={{ mb: 1, fontFamily: 'Roboto Condensed, sans-serif' }}>
-                                Course:
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary" sx={{ mb: 1, fontFamily: 'Roboto Condensed, sans-serif' }}>
-                                {post.course}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary" sx={{ mb: 1, fontFamily: 'Roboto Condensed, sans-serif' }}>
-                                Hole:
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary" sx={{ mb: 1, fontFamily: 'Roboto Condensed, sans-serif' }}>
-                                {post.hole}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary" sx={{ mb: 1, fontFamily: 'Roboto Condensed, sans-serif' }}>
-                                Type:
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary" sx={{ mb: 1, fontFamily: 'Roboto Condensed, sans-serif' }}>
-                                {post.type}
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button onClick={handleOpen} size="small" sx={{ fontFamily: 'Roboto Condensed, sans-serif', color: '#6EA15E' }}>Comments</Button>
-                        </CardActions>
-                        <Dialog open={open} onClose={handleClose} sx={{ "& .MuiPaper-root": { borderRadius: 2 } }}>
-                            <DialogTitle sx={{ fontFamily: 'Roboto Condensed, sans-serif'}} >Comments</DialogTitle>
-                            <DialogContent>
-                                <DialogContentText sx={{ fontFamily: 'Roboto Condensed, sans-serif' }}>
-                                    {/* {post.commentId} */}
-                                </DialogContentText>
-                                <TextField
-                                        autoFocus
-                                        margin="dense"
-                                        id="comment"
-                                        label="Comment"
-                                        type="text"
-                                        fullWidth
-                                        variant="standard"
-                                        InputLabelProps={{ style: { fontFamily: 'Roboto Condensed, sans-serif' } }}
-                                />
-                            </DialogContent>
-                            <DialogActions>
-                                <Button onClick={handleClose} variant="contained" sx={{ backgroundColor: '#6EA15E', ':hover': { backgroundColor: '#4B784A' }, fontFamily: 'Roboto Condensed, sans-serif', m: 0.5 }} >Add comment</Button>
-                                <Button onClick={handleClose} variant="contained" sx={{ backgroundColor: '#6EA15E', ':hover': { backgroundColor: '#4B784A' }, fontFamily: 'Roboto Condensed, sans-serif', m: 0.5 }} >Cancel</Button>
-                            </DialogActions>
-                        </Dialog>
-                    </Card>
-                </Grid>))}
-            </Grid>
+        <Container maxWidth="xl">
+            <Grid container spacing={4} sx={{ mt: 3 }}>
+                {posts.map((post, index) => (
+                    <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                        <Card sx={{ maxWidth: 300, borderRadius: 2, margin: '0 auto' }}>
+                            <CardMedia
+                                sx={{ height: 200 }}
+                                image="/images/discgolfaction.jpg"
+                                title="disc"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div" sx={{ mb: 1, fontFamily: 'Roboto Condensed, sans-serif' }}>
+                                    { post.isFound ? "Found" : "Lost" }
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary" sx={{ mb: 1, fontFamily: 'Roboto Condensed, sans-serif' }}>
+                                    <Typography component="span" sx={{ fontFamily: 'Roboto Condensed, sans-serif', fontWeight: 'bold' }}>Course:</Typography> {post.course}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary" sx={{ mb: 1, fontFamily: 'Roboto Condensed, sans-serif' }}>
+                                    <Typography component="span" sx={{ fontFamily: 'Roboto Condensed, sans-serif', fontWeight: 'bold' }}>Hole:</Typography> {post.hole}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary" sx={{ mb: 1, fontFamily: 'Roboto Condensed, sans-serif' }}>
+                                    <Typography component="span" sx={{ fontFamily: 'Roboto Condensed, sans-serif', fontWeight: 'bold' }}>Type:</Typography> {post.type}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button onClick={handleOpen} size="small" sx={{ fontFamily: 'Roboto Condensed, sans-serif', color: '#6EA15E' }}>Comments</Button>
+                            </CardActions>
+                            <Dialog open={open} onClose={handleClose} sx={{ "& .MuiPaper-root": { borderRadius: 2 } }}>
+                                <DialogTitle sx={{ fontFamily: 'Roboto Condensed, sans-serif'}} >Comments</DialogTitle>
+                                <DialogContent>
+                                    <DialogContentText sx={{ fontFamily: 'Roboto Condensed, sans-serif' }}>
+                                        {/* {post.commentId} */}
+                                    </DialogContentText>
+                                    <TextField
+                                            autoFocus
+                                            margin="dense"
+                                            id="comment"
+                                            label="Comment"
+                                            type="text"
+                                            fullWidth
+                                            variant="standard"
+                                            InputLabelProps={{ style: { fontFamily: 'Roboto Condensed, sans-serif' } }}
+                                    />
+                                </DialogContent>
+                                <DialogActions>
+                                    <Button onClick={handleClose} variant="contained" sx={{ backgroundColor: '#6EA15E', ':hover': { backgroundColor: '#4B784A' }, fontFamily: 'Roboto Condensed, sans-serif', m: 0.5 }} >Add comment</Button>
+                                    <Button onClick={handleClose} variant="contained" sx={{ backgroundColor: '#6EA15E', ':hover': { backgroundColor: '#4B784A' }, fontFamily: 'Roboto Condensed, sans-serif', m: 0.5 }} >Cancel</Button>
+                                </DialogActions>
+                            </Dialog>
+                        </Card>
+                    </Grid>))}
+                </Grid>
+        </Container>
         )
 }
 

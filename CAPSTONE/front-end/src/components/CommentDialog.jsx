32487369c536ff47f623628b371f1844c744e730
adam-sagar <adamschrissagar@gmail.com>
2023-04-26 16:12:42 +1200

    Moved dialog for comments to own component

---
+++ CAPSTONE/front-end/src/components/CommentDialog.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { Button } from '@mui/material';
+
+function CommentDialog() {
+
+    const [open, setOpen] = useState(false);
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+
+        <Dialog
+            open={open}
+            onClose={handleClose}
+        >
+            <DialogTitle sx={{ fontFamily: 'Roboto Condensed, sans-serif' }} >Comments</DialogTitle>
+            <DialogContent>
+                <DialogContentText sx={{ fontFamily: 'Roboto Condensed, sans-serif' }}>
+                    {/* {post.commentId} */}
+                </DialogContentText>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="comment"
+                    label="Comment"
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                    InputLabelProps={{ style: { fontFamily: 'Roboto Condensed, sans-serif' } }}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose} variant="contained" sx={{ backgroundColor: '#6EA15E', ':hover': { backgroundColor: '#4B784A' }, fontFamily: 'Roboto Condensed, sans-serif', m: 0.5 }} >Add comment</Button>
+                <Button onClick={handleClose} variant="contained" sx={{ backgroundColor: '#6EA15E', ':hover': { backgroundColor: '#4B784A' }, fontFamily: 'Roboto Condensed, sans-serif', m: 0.5 }} >Cancel</Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
+
+export default CommentDialog;
--- CAPSTONE/front-end/src/components/PostList.jsx
+++ CAPSTONE/front-end/src/components/PostList.jsx
@@ -1,29 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import axios from 'axios';
 import { Container, Grid } from '@mui/material';
+import CommentDialog from './CommentDialog';
 
 function PostList(props) {
 
-    const [open, setOpen] = useState(false);
-
     const handleOpen = () => {
         setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
     };
 
     return (
@@ -58,31 +46,7 @@ function PostList(props) {
                             <CardActions>
                                 <Button onClick={handleOpen} size="small" sx={{ fontFamily: 'Roboto Condensed, sans-serif', color: '#6EA15E' }}>Comments</Button>
                             </CardActions>
-                            <Dialog
-                                open={open}
-                                onClose={handleClose}
-                            > 
-                                <DialogTitle sx={{ fontFamily: 'Roboto Condensed, sans-serif' }} >Comments</DialogTitle>
-                                <DialogContent>
-                                    <DialogContentText sx={{ fontFamily: 'Roboto Condensed, sans-serif' }}>
-                                        {/* {post.commentId} */}
-                                    </DialogContentText>
-                                    <TextField
-                                        autoFocus
-                                        margin="dense"
-                                        id="comment"
-                                        label="Comment"
-                                        type="text"
-                                        fullWidth
-                                        variant="standard"
-                                        InputLabelProps={{ style: { fontFamily: 'Roboto Condensed, sans-serif' } }}
-                                    />
-                                </DialogContent>
-                                <DialogActions>
-                                    <Button onClick={handleClose} variant="contained" sx={{ backgroundColor: '#6EA15E', ':hover': { backgroundColor: '#4B784A' }, fontFamily: 'Roboto Condensed, sans-serif', m: 0.5 }} >Add comment</Button>
-                                    <Button onClick={handleClose} variant="contained" sx={{ backgroundColor: '#6EA15E', ':hover': { backgroundColor: '#4B784A' }, fontFamily: 'Roboto Condensed, sans-serif', m: 0.5 }} >Cancel</Button>
-                                </DialogActions>
-                            </Dialog>
+                            <CommentDialog />
                         </Card>
                     </Grid>))}
             </Grid>

5ca52f9c972c690f62db93e9db8452aa709a3d31
adam-sagar <adamschrissagar@gmail.com>
2023-05-02 22:29:10 +1200

    Added welcome text and animation to LandingPage

--- CAPSTONE/front-end/src/pages/LandingPage.jsx
+++ CAPSTONE/front-end/src/pages/LandingPage.jsx
@@ -7,10 +7,14 @@ function LandingPage() {
 
         <div className="LandingPage">
             <div className="welcome-text">
-                <div>
-                    <Link to="/login" className="link">Login</Link> or{" "}
-                    <Link to="/signup" className="link">Sign up</Link> here
-                </div>
+                <span className="welcome-header">Welcome to Disc Detective</span>
+                <p>An online platform for lost disc golf discs.
+                Whether you've lost your favorite disc or found one without an owner, 
+                Disc Detective connects you with fellow players looking to recover a
+                disc or reunite one with its owner. Search by course, hole, or type, 
+                and get back on the course in no time.</p>
+                <Link to="/login" className="link">Login</Link> or{" "}
+                <Link to="/signup" className="link">Sign up</Link> to begin your search.
             </div>
         </div>
     );
